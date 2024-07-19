dd71e37698f3070c04c3a5528a83835638392196
Caio Marques <mfr.caio@gmail.com>
2024-07-19 08:48:04 -0300

    Creating the input and select components

--- web/src/Pages/NewContact/index.js
+++ web/src/Pages/NewContact/index.js
@@ -1,7 +1,17 @@
+import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
+import Select from '../../components/Select';
 
 export default function NewContact() {
   return (
-    <PageHeader title='Novo Contato' />
+    <>
+      <PageHeader title='Novo Contato' />
+      <Input type='text' placeholder='Nome'/>
+      <Select>
+        <option value={1}>Teste 1</option>
+        <option value={2}>Teste 2</option>
+        <option value={3}>Teste 3</option>
+      </Select>
+    </>
   )
 }
