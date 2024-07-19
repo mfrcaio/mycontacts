8991eb801d17e5ce4a88f72785c6b8c93f0ea325
Caio Marques <mfr.caio@gmail.com>
2024-07-19 09:56:12 -0300

    Creating the Button component

--- web/src/Pages/NewContact/index.js
+++ web/src/Pages/NewContact/index.js
@@ -1,3 +1,4 @@
+import Button from '../../components/Button';
 import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
 import Select from '../../components/Select';
@@ -12,6 +13,8 @@ export default function NewContact() {
         <option value={2}>Teste 2</option>
         <option value={3}>Teste 3</option>
       </Select>
+      <Button type='button'>Salvar alterações</Button>
+      <Button type='button' disabled>Salvar alterações disabled</Button>
     </>
   )
 }
