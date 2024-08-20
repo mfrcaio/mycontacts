35df63a69e56e8e31aab407678eafb58c82f3232
Caio Marques <mfr.caio@gmail.com>
2024-08-20 15:08:31 -0300

    add api routes

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,18 @@
-const express = require("express");
+const express = require('express');
+require('express-async-errors');
+
+const routes = require('./routes');
 
 const app = express();
 
-app.get("/", (request, response) => {
-  response.send("Hello World");
+app.use(express.json());
+
+app.use(routes);
+
+app.use((error, request, response, next) => {
+    console.log('#### ERROR HANDLER');
+    console.log(error);
+    response.sendStatus(500);
 });
 
-app.listen(3000, () =>
-  console.log("🔥 Server started at http://localhost:3000")
-);
+app.listen(3000, () => console.log('Server started at http://localhost:3000'));
