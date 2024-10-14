d46877cc6811c33637cdfc9cdecbaba66d3639e0
Caio Marques <mfr.caio@gmail.com>
2024-10-14 15:31:38 -0300

    feat: adds the HttpClient service layer and refactor ContactsService to abstract it

---
+++ web/src/services/utils/HttpClient.js
@@ -0,0 +1,17 @@
+import delay from '../../utils/delay';
+
+class HttpClient {
+  constructor(baseUrl) {
+    this.baseUrl = baseUrl;
+  }
+
+  async get(path) {
+    const response = await fetch(`${this.baseUrl}${path}`);
+
+    await delay();
+
+    return response.json();
+  }
+}
+
+export default HttpClient;
