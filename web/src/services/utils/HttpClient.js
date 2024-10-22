434777c4ecb049b22fc25a9103a54e28547c94d6
Caio Marques <mfr.caio@gmail.com>
2024-10-22 15:29:20 -0300

    feat: create the class APIErrors to handle API errors and our custom error messages

--- web/src/services/utils/HttpClient.js
+++ web/src/services/utils/HttpClient.js
@@ -1,5 +1,5 @@
+import APIError from '../../errors/APIError';
 import delay from '../../utils/delay';
-
 class HttpClient {
   constructor(baseUrl) {
     this.baseUrl = baseUrl;
@@ -19,9 +19,7 @@ class HttpClient {
     if (response.ok)
       return body;
 
-    throw new Error(
-      body?.error || `${response.status} - ${response.statusText}`
-    );
+    throw new APIError(response, body);
   }
 }
 
